09f9307e26df6cc44c7e9922796163c84c7c1ee6
EatFinger <eatfinger@163.com>
2020-04-13 11:48:03 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,28 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'EatFinger',
+    age: 18,
+    students: [
+      {id: 110, name: 'Kobe', age: 30},
+      {id: 111, name: 'James', age: 18},
+      {id: 112, name: 'Curry', age: 28},
+      {id: 113, name: 'Eat', age: 36},
+    ],
+    counter: 0
   },
+  handleBtnClick(){
+    // 1. 错误做法，界面是不会刷新的
+    // this.data.counter++
+    // console.log(this.data.counter)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    // 2. this.setData()
+    this.setData({
+      counter: this.data.counter+1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction(){
+    this.setData({
+      counter: this.data.counter-1
+    })
   }
 })
